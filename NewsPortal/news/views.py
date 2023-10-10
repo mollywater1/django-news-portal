386dae350adf56a4b1034ba22c6792d654c04817
mollywater1 <ownyx@icloud.com>
2023-10-10 15:24:42 +0400

    added News and NewsList pages

--- NewsPortal/news/views.py
+++ NewsPortal/news/views.py
@@ -1,8 +1,20 @@
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from .models import Post
 
 class NewsList(ListView):
     model=Post
     ordering = 'id'
     template_name = 'newslist.html'
+    context_object_name = 'newslist'
+
+    def get_queryset(self):
+        queryset=Post.objects.filter(type='news')
+        return queryset
+class News(DetailView):
+    model = Post
+    ordering = 'id'
+    template_name = 'news.html'
     context_object_name = 'news'
+    def get_object(self, queryset=None):
+        obj=Post.objects.filter(type='news')
+        return obj
