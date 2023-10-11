b12612051f47af670f96be348a74a4da5edc12a0
mollywater1 <ownyx@icloud.com>
2023-10-11 11:39:33 +0400

    ())

--- NewsPortal/news/views.py
+++ NewsPortal/news/views.py
@@ -1,20 +1,38 @@
 from django.views.generic import ListView, DetailView
 from .models import Post
+from datetime import datetime
+
 
 class NewsList(ListView):
-    model=Post
+    model = Post
     ordering = 'id'
     template_name = 'newslist.html'
     context_object_name = 'newslist'
 
     def get_queryset(self):
-        queryset=Post.objects.filter(type='news')
+        queryset = Post.objects.filter(type='news')
         return queryset
-class News(DetailView):
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['time_now'] = datetime.utcnow()
+        return context
+
+
+class NewsDetail(DetailView):
     model = Post
     ordering = 'id'
     template_name = 'news.html'
     context_object_name = 'news'
+
     def get_object(self, queryset=None):
-        obj=Post.objects.filter(type='news')
+        obj = Post.objects.filter(type='news')
         return obj
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['time_now'] = datetime.utcnow()
+        return context
+    def get_queryset(self):
+        queryset = Post.objects.filter(type='news')
+        return queryset
